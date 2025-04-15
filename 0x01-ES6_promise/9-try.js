96a637968172b50c6741310e0a2bca20be95d6bf
Nicholas-Odiwuor <nickeyphillip155@gmail.com>
2025-04-15 17:02:08 +0000

    ES6 js.

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -2,13 +2,11 @@ export default function guardrail(mathFunction) {
   const queue = [];
   
   try {
-    const result = mathFunction();
-    queue.push(result);
-  } catch (error) {
-    queue.push(error.toString());
-  } finally {
-    queue.push('Guardrail was processed');
+    queue.push(mathFunction());
+  } catch (err) {
+    queue.push(`Error: ${err.message}`);
   }
-
+  queue.push('Guardrail was processed');
+  
   return queue;
 }
