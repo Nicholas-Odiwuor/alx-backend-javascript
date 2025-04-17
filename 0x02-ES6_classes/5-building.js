ff951f2da10a67ee69c36a5fe567aa60adfa016b
Nicholas-Odiwuor <nickeyphillip155@gmail.com>
2025-04-17 17:50:03 +0000

    ES6 Classes

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,30 +1,20 @@
 export default class Building {
   constructor(sqft) {
-    this.sqft = sqft; // Using setter
-    if (this.constructor !== Building && !this.evacuationWarningMessage) {
-      throw new Error(
-        'Class extending Building must override evacuationWarningMessage'
-      );
+    this.sqft = sqft;
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending override evacuationWarningMessage',
+        );
+      }
     }
   }
 
-  // Getter for sqft
   get sqft() {
     return this._sqft;
   }
 
-  // Setter for sqft
-  set sqft(newSqft) {
-    if (typeof newSqft !== 'number') {
-      throw new TypeError('sqft must be a number');
-    }
-    this._sqft = newSqft;
-  }
-
-  // Abstract method (to be overridden by child classes)
-  evacuationWarningMessage() {
-    throw new Error(
-      'Class extending Building must override evacuationWarningMessage'
-    );
+  set sqft(value) {
+    this._sqft = value;
   }
 }
