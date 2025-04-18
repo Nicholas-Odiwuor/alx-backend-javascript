ee2df0f10288057c29ac5bf3ab87ed3b0f39f148
Nicholas-Odiwuor <nickeyphillip155@gmail.com>
2025-04-18 16:19:21 +0000

    Classes

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -3,23 +3,12 @@ import Car from './10-car.js';
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
-    this.range = range;
+    this._range = range;
   }
 
-  // Getter and setter for range
-  get range() {
-    return this._range;
-  }
-
-  set range(newRange) {
-    if (typeof newRange !== 'string') {
-      throw new TypeError('Range must be a string');
-    }
-    this._range = newRange;
-  }
-
-  // Override Symbol.species to return Car instead of EVCar
-  static get [Symbol.species]() {
-    return Car;
+  cloneCar() {
+    const { _brand, _motor, _color } = this;
+    return new Car(_brand, _motor, _color);
   }
 }
+
