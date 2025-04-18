4915e80e312372a99636d1678a65d1c698e9a7e7
Nicholas-Odiwuor <nickeyphillip155@gmail.com>
2025-04-18 13:14:23 +0000

    Classes

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,51 +1,32 @@
-export default class ALXClass {
+export default class HolbertonClass {
   constructor(size, location) {
-    this.size = size;         // Using setter
-    this.location = location; // Using setter
+    this.size = size;
+    this.location = location;
   }
 
-  // Getter and setter for size
   get size() {
     return this._size;
   }
 
-  set size(newSize) {
-    if (typeof newSize !== 'number') {
-      throw new TypeError('Size must be a number');
-    }
-    this._size = newSize;
+  set size(value) {
+    this._size = value;
   }
 
-  // Getter and setter for location
   get location() {
     return this._location;
   }
 
-  set location(newLocation) {
-    if (typeof newLocation !== 'string') {
-      throw new TypeError('Location must be a string');
-    }
-    this._location = newLocation;
+  set location(value) {
+    this._location = value;
   }
 
-  // Number coercion - returns size
   [Symbol.toPrimitive](hint) {
     if (hint === 'number') {
-      return this._size;
+      return this.size;
     }
     if (hint === 'string') {
-      return this._location;
+      return this.location;
     }
-    return this._size; // default to number
-  }
-
-  // Explicit valueOf for number coercion (alternative approach)
-  valueOf() {
-    return this._size;
-  }
-
-  // Explicit toString for string coercion (alternative approach)
-  toString() {
-    return this._location;
+    return this;
   }
 }
