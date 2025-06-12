28a098fde1631eb5624ec785452381483a6d7e65
Nicholas-Odiwuor <nickeyphillip155@gmail.com>
2025-06-12 21:14:10 +0000

    Unittests

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,18 +1,40 @@
-const chai = require('chai');
-const expect = chai.expect;
-const calculateNumber = require('./1-calcul');
+// File: 1-calcul.test.js
+const assert = require('assert');
+const calculateNumber = require('./1-calcul.js');
 
-describe('calculateNumber with Chai', () => {
-  it('should return 4 when passed 1 and 3', () => {
-    expect(calculateNumber(1, 3)).to.equal(4);
+describe('calculateNumber', () => {
+  describe('SUM', () => {
+    it('should return 6 for SUM of 1.4 and 4.5', () => {
+      assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+    });
+
+    it('should return 5 for SUM of 1.2 and 3.7', () => {
+      assert.strictEqual(calculateNumber('SUM', 1.2, 3.7), 5);
+    });
   });
 
-  it('should return 5 when passed 1 and 3.7', () => {
-    expect(calculateNumber(1, 3.7)).to.equal(5);
+  describe('SUBTRACT', () => {
+    it('should return -4 for SUBTRACT of 1.4 and 4.5', () => {
+      assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+    });
+
+    it('should return 0 for SUBTRACT of 1.4 and 1.4', () => {
+      assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 1.4), 0);
+    });
   });
 
-  it('should return 6 when passed 1.5 and 3.7', () => {
-    expect(calculateNumber(1.5, 3.7)).to.equal(6);
+  describe('DIVIDE', () => {
+    it('should return 0.2 for DIVIDE of 1.4 and 4.5', () => {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+    });
+
+    it("should return 'Error' for DIVIDE of 1.4 and 0", () => {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+    });
+
+    it("should return 'Error' when b rounds to 0", () => {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0.2), 'Error');
+    });
   });
 });
 
