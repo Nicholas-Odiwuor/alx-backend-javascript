43ad9ad85b71ed245d1d0871b2d57e7178b8216a
Nicholas-Odiwuor <nickeyphillip155@gmail.com>
2025-06-12 21:25:27 +0000

    Unittests

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,6 +1,7 @@
 // File: 5-payment.test.js
 const sinon = require('sinon');
-const { expect } = require('chai');
+const assert = require('assert');
+const Utils = require('./utils');
 const sendPaymentRequestToAPI = require('./5-payment');
 
 describe('sendPaymentRequestToAPI', () => {
@@ -14,16 +15,16 @@ describe('sendPaymentRequestToAPI', () => {
     consoleSpy.restore();
   });
 
-  it('should log "The total is: 120" for inputs 100 and 20', () => {
+  it('should log "The total is: 120" once when called with 100 and 20', () => {
     sendPaymentRequestToAPI(100, 20);
-    expect(consoleSpy.calledOnce).to.be.true;
-    expect(consoleSpy.calledWithExactly('The total is: 120')).to.be.true;
+    assert(consoleSpy.calledOnce);
+    assert(consoleSpy.calledWithExactly('The total is: 120'));
   });
 
-  it('should log "The total is: 20" for inputs 10 and 10', () => {
+  it('should log "The total is: 20" once when called with 10 and 10', () => {
     sendPaymentRequestToAPI(10, 10);
-    expect(consoleSpy.calledOnce).to.be.true;
-    expect(consoleSpy.calledWithExactly('The total is: 20')).to.be.true;
+    assert(consoleSpy.calledOnce);
+    assert(consoleSpy.calledWithExactly('The total is: 20'));
   });
 });
 
