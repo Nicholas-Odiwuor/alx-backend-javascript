7f36b967dc6db08750264149637f00263e5e6135
Nicholas-Odiwuor <nickeyphillip155@gmail.com>
2025-06-12 21:44:08 +0000

    Unittests in JS

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,20 +1,37 @@
-// File: 8-api/api.test.js
 const request = require('request');
-const { expect } = require('chai');
+const chai = require('chai');
+const expect = chai.expect;
+const baseUrl = 'http://localhost:7865';
 
-describe('Index page', () => {
-  const url = 'http://localhost:7865';
-
-  it('should return status code 200', (done) => {
-    request.get(url, (err, res, body) => {
+describe('Index page', function () {
+  it('GET / should return 200 and correct body', function (done) {
+    request.get(`${baseUrl}/`, (err, res, body) => {
       expect(res.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
+
+describe('Cart page', function () {
+  it('GET /cart/12 should return 200 and correct body', function (done) {
+    request.get(`${baseUrl}/cart/12`, (err, res, body) => {
+      expect(res.statusCode).to.equal(200);
+      expect(body).to.equal('Payment methods for cart 12');
       done();
     });
   });
 
-  it('should return correct message', (done) => {
-    request.get(url, (err, res, body) => {
-      expect(body).to.equal('Welcome to the payment system');
+  it('GET /cart/hello should return 404', function (done) {
+    request.get(`${baseUrl}/cart/hello`, (err, res, body) => {
+      expect(res.statusCode).to.equal(404);
+      done();
+    });
+  });
+
+  it('GET /cart/12abc should return 404', function (done) {
+    request.get(`${baseUrl}/cart/12abc`, (err, res, body) => {
+      expect(res.statusCode).to.equal(404);
       done();
     });
   });
