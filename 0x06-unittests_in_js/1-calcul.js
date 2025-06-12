de9dbc42f0a863b4f1e8b123fd01c54ea724122a
Nicholas-Odiwuor <nickeyphillip155@gmail.com>
2025-06-12 21:12:36 +0000

    Unittests

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,5 +1,18 @@
-function calculateNumber(a, b) {
-  return Math.round(a) + Math.round(b);
+// File: 1-calcul.js
+function calculateNumber(type, a, b) {
+  const roundedA = Math.round(a);
+  const roundedB = Math.round(b);
+
+  if (type === 'SUM') {
+    return roundedA + roundedB;
+  } else if (type === 'SUBTRACT') {
+    return roundedA - roundedB;
+  } else if (type === 'DIVIDE') {
+    if (roundedB === 0) {
+      return 'Error';
+    }
+    return roundedA / roundedB;
+  }
 }
 
 module.exports = calculateNumber;
