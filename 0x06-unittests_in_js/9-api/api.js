0576da7d6b0b4a962966387f7ab51893afca0d3d
Nicholas-Odiwuor <nickeyphillip155@gmail.com>
2025-06-12 21:41:53 +0000

    JS Unittests

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -1,16 +1,15 @@
-
 const express = require('express');
-
 const app = express();
+
 const PORT = 7865;
 
-app.get('/', (_, res) => {
+app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
 
-app.get('/cart/:id(\\d+)', (req, res) => {
+// New regex-based route for cart
+app.get('/cart/:id([0-9]+)', (req, res) => {
   const id = req.params.id;
-
   res.send(`Payment methods for cart ${id}`);
 });
 
@@ -18,5 +17,5 @@ app.listen(PORT, () => {
   console.log(`API available on localhost port ${PORT}`);
 });
 
-module.exports = app;
+module.exports = app; // Export app for testing
 
